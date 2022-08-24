d9c0b3db590af2ad87af3fadc3976203b75c4176
Ekaterina Morozova <morozova1861@mail.ru>
2022-08-24 20:50:54 +0500

    task1.0

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+void NumsPositiv (int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){if(array[i] > 0)count += 1;}
+    Console.WriteLine($"В вашем списке положительных чисел {count}");
+}
+
+Console.WriteLine("Введите эелементы массива одной строкой через пробел");
+int[] myArray = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(string.Join(", ", myArray));
+NumsPositiv(myArray);
